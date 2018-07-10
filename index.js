7f5613c983008c4f6ae65d2badc8e04000094e98
Matt Shull <derekshull@gmail.com>
2018-07-10 00:52:14 -0500

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const boomcatch = require('boomcatch');
+
+boomcatch.listen({
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 5000,
+  https: false,
+  path: '/api/store-beacon',
+  referer: /.*/,
+  origin: '*',
+  limit: 100,
+  // maxSize: -1,
+  /*log: {
+      info: function () {},
+      warn: function () {},
+      error: function () {}
+  },*/
+  log: console,
+  validator: 'permissive',
+  filter: 'unfiltered',
+  mapper: 'unmapped',
+  forwarder: 'console',
+  workers: require('os').cpus().length,
+  delayRespawn: 0,
+  maxRespawn: -1
+});
