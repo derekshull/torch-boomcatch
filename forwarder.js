c700a6eca990fdaa34f26cd4660177e80f0df35b
Matt Shull <derekshull@gmail.com>
2018-07-11 22:56:02 -0500

    add beacon to DB

--- forwarder.js
+++ forwarder.js
@@ -3,10 +3,38 @@ const MongoClient = require('mongodb').MongoClient; // eslint-disable-line
 const url = 'mongodb://localhost:27017';
 const dbName = 'torch';
 
+function jsonify(obj) {
+  obj = JSON.parse(obj);
+  const data = obj.data;
+  // iterate over the property names
+  Object.keys(data).forEach(function(k) {
+    // slip the property value based on `.`
+    var prop = k.split('.');
+    // get the last value fom array
+    var last = prop.pop();
+    // iterate over the remaining array value 
+    // and define the object if not already defined
+    prop.reduce(function(o, key) {
+      // define the object if not defined and return
+      return o[key] = o[key] || {};
+      // set initial value as object
+      // and set the property value
+    }, data)[last] = data[k];
+    // delete the original property from object
+    delete data[k];
+  });
+
+  data['referer'] = obj['referrer'];
+  data['userAgent'] = obj['userAgent'];
+  data['browser'] = obj['browser'];
+
+  return data;
+}
+
 exports.initialise = function () {
   return function (data, type, separator, callback) {
     try {
-      const beaconData = JSON.parse(data);
+      const beaconData = jsonify(data);
       const bodyPid = beaconData.pid;
 
       MongoClient.connect(url, { useNewUrlParser: true } , (connectError, client) => {
